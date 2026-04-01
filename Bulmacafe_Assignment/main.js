78118b776e497ea731089d61bae380637fe7a244
dimaanojca-ai <dimaanoj.ca@gmail.com>
2026-04-01 18:16:35 -0400

    Add Bulmacafe Assignment

---
+++ Bulmacafe_Assignment/main.js
@@ -0,0 +1,8 @@
+// JS for mobile burger menu
+const burger = document.querySelector('.navbar-burger');
+const menu = document.querySelector('.navbar-menu');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('is-active');
+  menu.classList.toggle('is-active');
+});
